7b23b33d7c9577e3edfabded40b2a6d78b86a7ff
Nika Kotliar <wanderer2805@gmail.com>
2022-10-30 21:07:49 +0300

    123

---
+++ src/__tests__/test_creare Magician.js
@@ -0,0 +1,15 @@
+
+import {Magician,magician} from './Magician.js'
+
+test('creare Magician', () => {
+    const expected = {
+        _name: 'Magician',
+        _type: 'Magician',
+        attack: 10,
+        defence: 40,
+        health: 100,
+        level: 1
+    }
+    const received = new Magician("Magician", "Magician");
+    expect(received).toEqual(expected);
+});
---
+++ src/__tests__/test_creare Undead.js
@@ -0,0 +1,54 @@
+import { Undead, undead } from './Undead.js'
+
+test('creare Undead', () => {
+    const expected = {
+        _name: 'Undead',
+        _type: 'Undead',
+        attack: 25,
+        defence: 25,
+        health: 100,
+        level: 1
+    }
+    const received = new Undead("Undead", "Undead");
+    expect(received).toEqual(expected);
+});
+
+test('check Undead levelUp', () => {
+    const expected = {
+        _name: 'Undead',
+        _type: 'Undead',
+        attack: 43,
+        defence: 51,
+        health: 100,
+        level: 4
+    }
+    const received = new Undead("Undead", "Undead");
+    received.levelUp()
+    received.levelUp()
+    received.levelUp()
+
+    expect(received).toEqual(expected);
+});
+
+test('check Undead damage', () => {
+    const expected = {
+        _name: 'Undead',
+        _type: 'Undead',
+        attack: 25,
+        defence: 25,
+        health: -50,
+        level: 1
+    }
+    const received = new Undead("Undead", "Undead");
+    received.damage(200)
+    expect(received).toEqual(expected);
+});
+
+test('check Undead levelUp with health lower 0', () => {
+    const received = new Undead("Undead", "Undead");
+    received.damage(200)
+    received.levelUp()
+
+    expect(received).toThrow("нельзя повысить левел умершего");
+
+});
